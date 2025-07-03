8709dd0afc459d4dd3ef02fedc2a02bc542b08ac
Anton Panfilov <anton@panfilov.biz>
2025-07-02 20:56:09 -0700

    special error text if there are no available parameters

--- src/Macros/Helper/AllowedParamsValidator.php
+++ src/Macros/Helper/AllowedParamsValidator.php
@@ -9,14 +9,19 @@ trait AllowedParamsValidator
     public function validateParam(?string $param): bool|Error
     {
         $allowed_params = $this->paramAllowOnly();
-        return in_array($param, $allowed_params)
-            ? true
-            : new Error(
-                "invalid param `$param`, allowed only: `" .
+
+        if (in_array($param, $allowed_params)) {
+            return true;
+        }
+
+        return new Error(
+            empty($allowed_params)
+                ? 'there are no available parameters'
+                : "invalid param `$param`, allowed only: `" .
                 implode(
                     "`, `",
                     $allowed_params
                 ) . "`"
-            );
+        );
     }
 }
